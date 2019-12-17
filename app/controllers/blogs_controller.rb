7d1f66b41dbc273efd293e9fe3bb61a62ee4f23c
Hatem <hatem.xhezairi96@gmail.com>
2019-12-17 12:03:09 +0100

    Built pages with controller generator

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,15 @@
+class BlogController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @blogs =Blog.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @blog = Blog.new
+  end
