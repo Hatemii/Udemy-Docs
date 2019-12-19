4ef35358ab1f76bf14c859c9d447978e62c61036
Hatemii <hatem.xhezairi96@gmail.com>
2019-12-19 14:46:49 +0100

    Implemented custom scopes for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_130239) do
+ActiveRecord::Schema.define(version: 2019_12_19_130837) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 2019_12_19_130239) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2019_12_19_130239) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "blogs", "topics"
 end
